51720a0d4791336ea954b7516d02c5a2093401ad
maxbelokur <mv.belokur@yandex.ru>
2025-02-03 14:09:31 +0400

    Update send_tg_notification.py

--- .github/workflows/send_tg_notification.py
+++ .github/workflows/send_tg_notification.py
@@ -2,13 +2,22 @@ import requests
 import os
 
 # Получаем переменные окружения из GitHub Secrets
-TELEGRAM_BOT_TOKEN = os.getenv("7993106648:AAG4PW_3pKZr6nnYg3Mhl1l937utNTeKv3w")
-TELEGRAM_CHAT_ID = os.getenv("179526632")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_message(message):
+    if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
+        print("❌ Ошибка: TELEGRAM_BOT_TOKEN или TELEGRAM_CHAT_ID не найдены!")
+        return
+    
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     data = {"chat_id": TELEGRAM_CHAT_ID, "text": message, "parse_mode": "Markdown"}
     requests.post(url, data=data)
+    response = requests.post(url, data=data)
+    
+    if response.status_code == 200:
+        print("Сообщение отправлено!")
+    else:
+        print(f"❌ Ошибка при отправке: {response.text}")
 
-# Отправка уведомления
 send_message("*Форматирование CSV успешно завершено!* \n\nИзменения загружены в репозиторий.")
