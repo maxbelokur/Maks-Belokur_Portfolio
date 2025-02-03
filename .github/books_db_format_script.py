f7d975289c00553dd22ce584580e3c29362dca9a
maxbelokur <mv.belokur@yandex.ru>
2025-02-03 18:51:34 +0400

    Update books_db_format_script.py
    Exclude data types format.

--- .github/books_db_format_script.py
+++ .github/books_db_format_script.py
@@ -9,13 +9,6 @@ df = pd.read_csv(input_file, sep=',')
 # Заполнение пропущенных (NaN) значение 
 df.fillna({'purchase_price': 0, 'city': 'Неизвестно'}, inplace = True)
 
-# Форматирование типов данных для более удобнойработой с данными и сокращение размера файла.
-df['date'] = pd.to_datetime(df['date'])
-df['user_id'] = df['user_id'].astype('int16')
-df['book_id'] = df['book_id'].astype('int16')
-df['time_spent_minutes'] = df['time_spent_minutes'].astype('int16')
-df['purchase_price'] = df['purchase_price'].astype('float16')
-
 # Сортировка датафрейма по возрастанию даты и ID пользователя 
 df = df.sort_values(['date', 'user_id'], ascending=True)
 
