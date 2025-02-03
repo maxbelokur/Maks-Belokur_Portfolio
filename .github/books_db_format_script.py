53750634bcf0d8b17d54c52e30476df434db286d
maxbelokur <mv.belokur@yandex.ru>
2025-02-03 18:36:30 +0400

    Update books_db_format_script.py

--- .github/books_db_format_script.py
+++ .github/books_db_format_script.py
@@ -2,10 +2,7 @@ import os
 import pandas as pd
 
 input_file = ".github/books_database_main.csv"
-output_file = ".books_database_FORMATED.csv"
-
-if not os.path.exists(output_file):
-  pd.DataFrame().to_csv(output_file, index=False)
+output_file = ".github/books_database_FORMATED.csv"
 
 df = pd.read_csv(input_file, sep=',')
 
@@ -23,4 +20,6 @@ df['purchase_price'] = df['purchase_price'].astype('float16')
 df = df.sort_values(['date', 'user_id'], ascending=True)
 
 # Запись отформатированного датафрейма в новый файл.
+print(f"Размер датафрейма перед записью: {df.shape}")
 df.to_csv(output_file, index=False)
+print(f"Файл {output_file} успешно сохранен!")
