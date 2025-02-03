126ca5e0c912826614f71331563a357976b21574
maxbelokur <mv.belokur@yandex.ru>
2025-02-03 19:14:25 +0400

    Update send_tg_notification.py

--- .github/workflows/send_tg_notification.py
+++ .github/workflows/send_tg_notification.py
@@ -7,11 +7,12 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 message = " ".join(sys.argv[1:]) if len(sys.argv) > 1 else os.getenv("MESSAGE", "GitHub Action завершен!")
+escaped_message = message.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
 
 def send_message(message):
     """Отправка сообщения в Telegram"""
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    data = {"chat_id": TELEGRAM_CHAT_ID, "text": message, "parse_mode": "Markdown"}
+    data = {"chat_id": os.getenv("TELEGRAM_CHAT_ID"), "text": escaped_message, "parse_mode": "HTML"}
     requests.post(url, data=data)
     response = requests.post(url, data=data)
     
