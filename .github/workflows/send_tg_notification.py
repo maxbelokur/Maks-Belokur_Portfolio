d2da353b196c3e9497fac23c0e13bef3ed7b204a
maxbelokur <mv.belokur@yandex.ru>
2025-02-03 19:02:57 +0400

    Update send_tg_notification.py
    make TG script common for all actions in this repository.

--- .github/workflows/send_tg_notification.py
+++ .github/workflows/send_tg_notification.py
@@ -5,11 +5,10 @@ import os
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
+message = " ".join(sys.argv[1:]) if len(sys.argv) > 1 else os.getenv("MESSAGE", "GitHub Action завершен!")
+
 def send_message(message):
-    if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
-        print("❌ Ошибка: TELEGRAM_BOT_TOKEN или TELEGRAM_CHAT_ID не найдены!")
-        return
-    
+    """Отправка сообщения в Telegram"""
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     data = {"chat_id": TELEGRAM_CHAT_ID, "text": message, "parse_mode": "Markdown"}
     requests.post(url, data=data)
@@ -20,4 +19,4 @@ def send_message(message):
     else:
         print(f"❌ Ошибка при отправке: {response.text}")
 
-send_message("*Форматирование Books_database.CSV успешно завершено!* \n\nИзменения загружены в репозиторий в файл books_database_FORMATED.csv.")
+send_message(message)
