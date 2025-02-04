c4e2f3e07c890795a37b8f3eacaa505175c58ff4
maxbelokur <mv.belokur@yandex.ru>
2025-02-04 11:00:36 +0400

    Update send_tg_notification.py
    delete second requests.post

--- .github/workflows/send_tg_notification.py
+++ .github/workflows/send_tg_notification.py
@@ -13,7 +13,6 @@ def send_message(message):
     """Отправка сообщения в Telegram"""
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     data = {"chat_id": os.getenv("TELEGRAM_CHAT_ID"), "text": escaped_message, "parse_mode": "HTML"}
-    requests.post(url, data=data)
     response = requests.post(url, data=data)
     
     if response.status_code == 200:
