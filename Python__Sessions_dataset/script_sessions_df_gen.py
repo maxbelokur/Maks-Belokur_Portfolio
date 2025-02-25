9be32402c1362958ee134505a5248a2d45651ced
Maksim Belokur <mv.belokur@yandex.ru>
2025-02-25 18:51:55 +0400

    Update script_sessions_df_gen.py
    fix device generation
    update funnel retention generation

--- Python__Sessions_dataset/script_sessions_df_gen.py
+++ Python__Sessions_dataset/script_sessions_df_gen.py
@@ -17,17 +17,19 @@ time_range = pd.date_range(start="2024-01-01 06:00:00", end="2024-12-31 23:59:00
 session_start = np.random.choice(time_range, size=num_sessions)
 session_end = session_start + pd.to_timedelta(np.random.randint(1, 1000, num_sessions), unit='s')
 
+devices = ['iOS', 'Android', 'Chrome', 'InternetExplorer', 'YandexBrowser']
+
 # Симуляция воронки.
 level_1 = np.random.choice([1, 0], size=num_sessions, p=[0.9, 0.1])  # 90% reach level 1
-level_2 = np.where(level_1 == 1, np.random.choice([1, 0], size=num_sessions, p=[0.7, 0.3]), 0)  # 70% of L1 reach L2
-level_3 = np.where(level_2 == 1, np.random.choice([1, 0], size=num_sessions, p=[0.5, 0.5]), 0)  # 50% of L2 reach L3
-level_4 = np.where(level_3 == 1, np.random.choice([1, 0], size=num_sessions, p=[0.3, 0.7]), 0)  # 30% of L3 reach L4
-level_5 = np.where(level_4 == 1, np.random.choice([1, 0], size=num_sessions, p=[0.2, 0.8]), 0)  # 20% of L4 reach L5
+level_2 = np.where(level_1 == 1, np.random.choice([1, 0], size=num_sessions, p=[0.82, 0.18]), 0)  # 70% of L1 reach L2
+level_3 = np.where(level_2 == 1, np.random.choice([1, 0], size=num_sessions, p=[0.63, 0.37]), 0)  # 50% of L2 reach L3
+level_4 = np.where(level_3 == 1, np.random.choice([1, 0], size=num_sessions, p=[0.5, 0.5]), 0)  # 30% of L3 reach L4
+level_5 = np.where(level_4 == 1, np.random.choice([1, 0], size=num_sessions, p=[0.33, 0.67]), 0)  # 20% of L4 reach L5
 
 # Сведение данных в датафрейм.
 df = pd.DataFrame({
     'user_id': user_ids,
-    'device': np.random.choice(['iOS', 'Android', 'Chrome', 'Internet Explorer', '']),
+    'device': np.random.choice(devices, size=num_sessions, p=[0.385, 0.285, 0.135, 0.085, 0.110]),
     'session_start': session_start,
     'session_end': session_end,
     'funnel_level_1': level_1,
